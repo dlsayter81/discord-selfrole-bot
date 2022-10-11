3be066db13e7ba96de5cdb6cc0bc2cf962115ae2
dlsayter81 <61679408+dlsayter81@users.noreply.github.com>
2022-10-11 07:13:06 -0500

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -3,30 +3,22 @@ module.exports = {
     description: "Sets up a reaction role message!",
     async execute(message, args, Discord, client) {
         const channel = 'CHANNEL-ID';
-        const role_1 = message.guild.roles.cache.find(role => role.name === "ROLE-NAME-1");
-        const role_2 = message.guild.roles.cache.find(role => role.name === "ROLE-NAME-2");
-        const role_3 = message.guild.roles.cache.find(role => role.name === "ROLE-NAME-3");
-        const role_4 = message.guild.roles.cache.find(role => role.name === "ROLE-NAME-4");
+        const role_1 = message.guild.roles.cache.find(role => role.name === "Muted");
+
                 
         const role_1_emoji='🥼';
-        const role_2_emoji='🎨';
-        const role_3_emoji='😎';
-        const role_4_emoji='🤓';
+
 
         let embed = new Discord.MessageEmbed()
         .setColor('#e42643')
-        .setTitle('YOUR TITLE HERE')
-        .setDescription('YOUR DESCRIPTION HERE ! \n\n'
-        + '🤓 for ROLE 4\n'
-        + '😎 for ROLE 3\n'
+        .setTitle('Dumb')
+        .setDescription('Do Not be Stupid and click this. The result will be unchangable. \n\n'
+
         + '🥼 for ROLE 1\n'
-        + '🎨 for ROLE 2\n')
 
         let messageEmbed = await message.channel.send(embed);
         messageEmbed.react(role_1_emoji);
-        messageEmbed.react(role_2_emoji);
-        messageEmbed.react(role_3_emoji);
-        messageEmbed.react(role_4_emoji);
+
 
         client.on('messageReactionAdd', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
@@ -38,15 +30,8 @@ module.exports = {
                 if (reaction.emoji.name === role_1_emoji) {
                     await reaction.message.guild.members.cache.get(user.id).roles.add(role_1);
                 }
-                else if (reaction.emoji.name === role_2_emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(role_2);
-                }
-                else if (reaction.emoji.name === role_3_emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(role_3);
-                }
-                else if (reaction.emoji.name === role_4_emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(role_4);
-                }
+            
+            
 
             } else {
                 return;
@@ -64,17 +49,7 @@ module.exports = {
             if (reaction.message.channel.id == channel) {
                 if (reaction.emoji.name === role_1_emoji) {
                     await reaction.message.guild.members.cache.get(user.id).roles.remove(role_1);
-                }
-                else if (reaction.emoji.name === role_2_emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(role_2);
-                }
-                else if (reaction.emoji.name === role_3_emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(role_3);
-                }
-                else if (reaction.emoji.name === role_4_emoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(role_4);
-                }
-
+              
             } else {
                 return;
             }
